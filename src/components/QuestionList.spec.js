a12a29e27370e925cbfb0a11f84764e8e6705a31
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-05-12 18:55:35 -0400

    added async tests

--- src/components/QuestionList.spec.js
+++ src/components/QuestionList.spec.js
@@ -1,5 +1,16 @@
 describe('The Question List Component', () => {
-    it('should display a list of items', () => {
+    beforeAll(() => {
+        console.log("Before all!")
+    })
+    beforeEach(() => {
+        console.log("Before each!")
+    })
+
+    it.skip('should display a list of items', () => {
         expect(2 + 2).toEqual(4)
+    })
+
+    it('should display a list of items', () => {
+        expect(2 + 3).toEqual(5)
     })
 })
