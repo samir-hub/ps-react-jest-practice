6f9b7ab572c995fa013ed422e230e342a39aa6bc
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-05-14 14:53:15 -0400

    done with mocks

--- src/sagas/fetch-question-saga.spec.js
+++ src/sagas/fetch-question-saga.spec.js
@@ -1,7 +1,14 @@
-import { handleFetchQuestion } from './fetch-question-saga';
+import { handleFetchQuestion } from "./fetch-question-saga";
+import fetch from "isomorphic-fetch";
 
 describe("Fetch questions saga", () => {
-    it("should fetch the questions", () => {
-        console.log("Test running")
-    })
-})
+  beforeAll(() => {
+    fetch.__setValue([{ question_id: 42 }]);
+  });
+  it("should fetch the questions", async () => {
+    const gen = handleFetchQuestion({ question_id: 42 });
+    const { value } = await gen.next();
+    expect(value).toEqual([{ question_id: 42 }]);
+    expect(fetch).toHaveBeenCalledWith("/api/questions/42");
+  });
+});
