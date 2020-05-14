8a122af6fd7e222ebf2b02ddfb7022b7c70699f8
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-05-14 14:33:05 -0400

    adding mocks

---
+++ src/sagas/fetch-question-saga.spec.js
@@ -0,0 +1,7 @@
+import { handleFetchQuestion } from './fetch-question-saga';
+
+describe("Fetch questions saga", () => {
+    it("should fetch the questions", () => {
+        console.log("Test running")
+    })
+})
