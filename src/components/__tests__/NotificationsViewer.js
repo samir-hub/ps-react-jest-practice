b506182156fc196d3b771d3936b8c1b384c3cb51
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-05-24 23:38:49 -0400

    wrote test version 1

--- src/components/__tests__/NotificationsViewer.js
+++ src/components/__tests__/NotificationsViewer.js
@@ -1,1 +1,20 @@
-describe()
+import NotificationsViewer from '../NotificationsViewer';
+import renderer from 'react-test-renderer'; 
+import React from 'react'; 
+import delay from 'redux-saga'; 
+
+describe("The notification viewer", () => {
+    it("should show the correct number", async() => {
+        const tree = renderer
+        .create(
+            <NotificationsViewer/>
+        )
+
+        await delay();
+        const instance = tree.root; 
+        const component = instance.findByProps({className: 'notifications'})
+        const text = component.children[0]
+
+        expect(text).toEqual("5 Notifications")
+    })
+})
