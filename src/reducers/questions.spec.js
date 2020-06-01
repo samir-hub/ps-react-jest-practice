89ad332a5491db9168a71a3641cb3d01ce481dc8
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-06-01 18:30:03 -0400

    added reducer test

--- src/reducers/questions.spec.js
+++ src/reducers/questions.spec.js
@@ -1,5 +1,11 @@
+import { questions } from './questions'; 
+
 describe("The questions reducer", () => {
     it("should work", ()=>{
-        
+        console.log("Testing questions");
+        const state = ["foo", "bar"]
+        const newState = questions(state, {type: "UNRECOGNIZED_ACTION"});
+
+        expect(newState).toEqual(state); 
     })
 })
